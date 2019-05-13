a6cdd4d8a2655abcdc3b277fb3e30fe9ac2a1769
VPC9528 <VPC9528@Z908FNBQ2139.uniqa.at>
2019-05-13 13:43:03 +0200

    cart table partially working

--- html5.css3.js2/src/main/webapp/js/onlineshop.js
+++ html5.css3.js2/src/main/webapp/js/onlineshop.js
@@ -82,47 +82,53 @@ function setAddedToCart(elem) {
 	if (elem.checked) {
 		elem.nextElementSibling.innerHTML = "Added to cart!"; // the first sibling of the checkbox is the <span> element
 		elem.nextElementSibling.classList.add("addedToCart"); // add a css class to an element, @see https://stackoverflow.com/a/16283612/1925356
-		addToCartTable(elem);
 	} else {
 		elem.nextElementSibling.innerHTML = "Add to cart"; // the first sibling of the checkbox is the <span> element
 		elem.nextElementSibling.classList.remove("addedToCart"); // remove a css class to an element, @see https://stackoverflow.com/a/16283612/1925356
 	}
+
+	addOrRemoveCartItem(elem);
 }
 
 /**
  * @returns
  */
-function addToCartTable(elementToAdd) {
+function addOrRemoveCartItem(elementToAdd) {
 
-	var cartTableDiv = document.getElementById("cartTableDiv");
+	var cartTable = document.getElementById("cartTableId");
+	var rowNo = cartTable.rows.length;
 
-	if (cartTableDiv == null) {
-		cartTableDiv = document.createElement("div");
-		cartTableDiv.style.width = "15%";
-		cartTableDiv.style.float = "right";
-		document.getElementById("leftDiv").width = "20%";
-
-		var rightDiv = document.getElementById("rightDiv");
-		rightDiv.width = "65%";
-		rightDiv.style.float="none";
-		rightDiv.style.display = "inline";
-		document.body.appendChild(cartTableDiv);
-
-		var cartTable = document.createElement("table");
-		cartTableDiv.appendChild(cartTable);
-
+	if (elementToAdd.checked) {
 		var cartTableRow1 = document.createElement("tr");
+		cartTableRow1.id = "cartTableId_" + elementToAdd.id;
 		cartTable.appendChild(cartTableRow1);
 
+		var cartTableTD0 = document.createElement("td");
+		cartTableTD0.innerHTML = (rowNo + 1);
+		cartTableRow1.appendChild(cartTableTD0);
+
+		var productName = document.createTextNode(elementToAdd.value);
 		var cartTableTD1 = document.createElement("td");
+		cartTableTD1.appendChild(productName);
 		cartTableRow1.appendChild(cartTableTD1);
 
 		var cartTableTD2 = document.createElement("td");
+		var priceValue = elementToAdd.nextElementSibling.nextElementSibling.innerHTML;
+
+		var priceNode = document.createTextNode(priceValue);
+		cartTableTD2.appendChild(priceNode);
 		cartTableRow1.appendChild(cartTableTD2);
 
-		var productName = document.createTextNode(elementToAdd.value);
-		cartTableRow1.appendChild(productName);
+	} else {
+		var rowToRemove = document.getElementById("cartTableId_" + elementToAdd.id);
+		var rowToRemoveIndex = parseInt(rowToRemove.firstElementChild.id, 10);
+		cartTable.deleteRow(rowToRemoveIndex - 1);
+		if (rowToRemoveIndex < cartTable.rows.length) {
+			var i;
+			for (i = 0; i < cartTable.rows.length; i++) {
+				cartTable[0].firstElementChild.innerHTML = (i + 1);
+			}
+		}
 
 	}
-
-}
+};
